22027077afbbc4aa9404187d38a235de5297009b
Ranidhumma <ranidhumma8@gmail.com>
2022-04-06 19:56:30 +0530

    Edited contact by Name

--- src/main/java/com/java/AddressbookMain.java
+++ src/main/java/com/java/AddressbookMain.java
@@ -1,20 +1,89 @@
 package com.java;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class AddressbookMain {
 
-	private static Scanner sc = new Scanner(System.in);
+	private static Scanner scanner = new Scanner(System.in);
+	/**
+	 * create object for ContactService class object name as contactService
+	 */
+	static ContactService contactService;
+	/**
+	 * create object for AddressBook class object name as addressBook
+	 */
+	static AddressBook addressBook;
+
+	/**
+	 * create a method name as findContact
+	 */
+	public static void findContact() {
+		System.out.println("Enter Person Name");
+		String name = scanner.nextLine();
+		/**
+		 * search this person by name in address book and display contact
+		 */
+		contactService.displayContact(addressBook.searchByName(name));
+	}
+
+	public static void editContact() {
+
+		System.out.println("Enter Person Name");
+		String name = scanner.nextLine();
+		ContactPerson contact = addressBook.searchByName(name);
+
+		if (Objects.nonNull(contact)) {
+			/**
+			 * calling editExistingContact method from contactService object
+			 */
+			contactService.editExistingContact(contact);
+			return;
+		}
+		System.out.println("Person Not Found");
+	}
+
+	public static void createContact() {
+
+		addressBook.addContact(contactService.createContact());
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Workshop");
-		ContactService contactService = new ContactService(sc);
-		ContactPerson contact = new ContactPerson();
-		contactService.createPerson(contact, sc);
-		System.out.println(contact + "\n");
-		AddressBook addressBook = new AddressBook();
-		addressBook.addContact(contact);
-		System.out.println("Contact is added into the addressBook");
+		addressBook = new AddressBook();
+		contactService = new ContactService(scanner);
+		while (true) {
+			System.out.println(
+					"\nAddress Book Options:\n1 (Add new contact)\n2 (Edit existing contact)\n3 (View all contact)\n4 (Find contact)\n5 Exit");
+			int option = Integer.parseInt(scanner.nextLine());
 
+			switch (option) {
+
+			case 1:
+				createContact();
+				break;
+
+			case 2:
+				editContact();
+				break;
+
+			case 3:
+
+				System.out.println(addressBook);
+				break;
+
+			case 4:
+
+				findContact();
+				break;
+
+			case 5:
+				System.out.println("Exit");
+				break;
+
+			default:
+				System.out.println("Invalid");
+			}
+		}
 	}
 }
--- src/main/java/com/java/ContactService.java
+++ src/main/java/com/java/ContactService.java
@@ -3,23 +3,21 @@ package com.java;
 import java.util.Scanner;
 
 public class ContactService {
-
 	private Scanner sc;
 	private Validator validator;
 
 	/**
 	 * create a parameterized constructor name as ContactService
-	 * 
-	 * @param sc- scanner i/p
 	 */
+
 	public ContactService(Scanner sc) {
 
 		this.sc = sc;
 		this.validator = new Validator();
 	}
 
-	public void createPerson(ContactPerson contact, Scanner sc) {
-
+	public ContactPerson createContact() {
+		ContactPerson contact = new ContactPerson();
 		/**
 		 * variables in all string data type
 		 */
@@ -193,5 +191,83 @@ public class ContactService {
 			}
 		}
 		System.out.println("\nContact is added successfully\n");
+		return contact;
+
+	}
+
+	public void editExistingContact(ContactPerson contact) {
+		while (true) {
+
+			System.out.println(
+					"1 (FirstName)\n2 (LastName)\n3 (Address)\n4 (City)\n5 (State)\n6 (Zip)\n7 (Phone)\n8 (Email)\n9 (Exit)");
+			int options = Integer.parseInt(sc.nextLine());
+
+			switch (options) {
+
+			case 1:
+				System.out.println("Enter New FirstName");
+				String newFirstName = sc.nextLine();
+				contact.setFirstName(newFirstName);
+				break;
+
+			case 2:
+				System.out.println("Enter New LastName");
+				String newLastName = sc.nextLine();
+				contact.setLastName(newLastName);
+				break;
+
+			case 3:
+				System.out.println("Enter New Address");
+				String newAddress = sc.nextLine();
+				contact.setAddress(newAddress);
+				break;
+
+			case 4:
+				System.out.println("Enter New City");
+				String newCity = sc.nextLine();
+				contact.setCity(newCity);
+				break;
+
+			case 5:
+				System.out.println("Enter State");
+				String newState = sc.nextLine();
+				contact.setState(newState);
+				break;
+
+			case 6:
+				System.out.println("Enter New Zip");
+				String newZip = sc.nextLine();
+				contact.setZip(newZip);
+				break;
+
+			case 7:
+				System.out.println("Enter New Phone");
+				String newPhone = sc.nextLine();
+				contact.setPhoneNumber(newPhone);
+				break;
+
+			case 8:
+				System.out.println("Enter New Email");
+				String newEmail = sc.nextLine();
+				contact.setFirstName(newEmail);
+				break;
+
+			case 9:
+				System.out.println("Exit");
+				return;
+
+			default:
+				System.out.println("Invalid");
+				break;
+			}
+		}
+	}
+
+	public void displayContact(ContactPerson contact) {
+		if (contact == null) {
+			System.out.println("No contact found");
+			return;
+		}
+		System.out.println(contact);
 	}
 }
